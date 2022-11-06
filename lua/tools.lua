8acf08cc41a033146609a6d5a2a243726b1b08f3
Fermin <ferminoloriz@gmail.com>
2022-11-05 22:14:36 -0600

    Build command improved

--- lua/tools.lua
+++ lua/tools.lua
@@ -1,9 +1,14 @@
 local M = {}
 
 function M.build()
-    --print(vim.api.nvim_buf_get_name(0))
-    print("build function in lua module tools")
-    vim.cmd('9sp term://w:\\handmade\\code\\build')
+    local result = vim.fn.system('dir build.bat /s/b')
+    if string.find(result, " ") then
+        -- 'File Not Found' would be the return value in this case
+        print("No build script found.")
+        return
+    end
+    result = '9sp term://'..result
+    vim.cmd(result) 
 end
 
 return M
